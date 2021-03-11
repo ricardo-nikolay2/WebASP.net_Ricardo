8645efddb2321976c6f77d011f855420bb4b9a89
wahyu <wahyu@istw.co.id>
2021-03-12 03:43:19 +0700

    commit awal

---
+++ Web_MVC_NET_5/Data/Migrations/20210311103419_FirstInitial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web_MVC_NET_5.Data.Migrations
+{
+    public partial class FirstInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

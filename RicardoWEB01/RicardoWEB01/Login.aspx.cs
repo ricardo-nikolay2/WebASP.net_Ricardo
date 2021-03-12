6514b23fd71adc06d2d7c5958b3e5326ab2135a0
ricardo-nikolay2 <ricardo.nikolay@gmail.com>
2021-03-12 13:24:39 +0700

    menambahkan form Login.aspx dan validasi dengan database

---
+++ RicardoWEB01/RicardoWEB01/Login.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace RicardoWEB01
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Btn_login_Click(object sender, EventArgs e)
+        {
+            SqlConnection RicardoConn = new SqlConnection(ConfigurationManager.ConnectionStrings["RicardoDB01Conn"].ConnectionString);
+            RicardoConn.Open();
+            string cekuser = "SELECT COUNT(*) FROM [User] WHERE username = '" + UserLogin.Text + "'";
+            SqlCommand query = new SqlCommand(cekuser, RicardoConn);
+            int Jumlah = Convert.ToInt32(query.ExecuteScalar().ToString());
+            RicardoConn.Close();
+
+            if (Jumlah == 1)
+            {
+                RicardoConn.Open();
+                string passwordsql = "SELECT password FROM [user] WHERE username='" + UserLogin.Text + "' ";
+                SqlCommand passquery = new SqlCommand(passwordsql, RicardoConn);
+                string password = passquery.ExecuteScalar().ToString();
+                RicardoConn.Close();
+
+                if (password == PassLogin.Text)
+                {
+                    //session 
+                    Response.Redirect("Admin.aspx");
+                }
+                else
+                {
+                    Response.Write("password salah!");
+                }
+            }
+
+            else
+            {
+                Response.Write("Username tidak ditemukan ");
+            }
+        }
+    }
+}
